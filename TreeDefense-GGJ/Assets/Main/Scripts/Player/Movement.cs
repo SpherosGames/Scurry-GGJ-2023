b8ad1477bc409149c2e74d1d436a4858e09b5565
Niek Melet <nmelet@st.noorderpoort.nl>
2023-02-03 23:38:05 +0100

    Added Spring

--- TreeDefense-GGJ/Assets/Main/Scripts/Player/Movement.cs
+++ TreeDefense-GGJ/Assets/Main/Scripts/Player/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
+    public float sprintSpeed;
 
     public void Start()
     {
@@ -12,12 +13,18 @@ public class Movement : MonoBehaviour
     }
     void Update()
     {
-       transform.Translate(Vector2.right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
-
+       SpeedMovement();
+       
+       if(Input.GetButton("Fire3"))
+       {
+           transform.Translate(Vector2.right * moveSpeed * sprintSpeed* Time.deltaTime * Input.GetAxis("Horizontal"));
+       }
     
     }
-       
-    
-        
-       
+
+    void SpeedMovement()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+
 }
