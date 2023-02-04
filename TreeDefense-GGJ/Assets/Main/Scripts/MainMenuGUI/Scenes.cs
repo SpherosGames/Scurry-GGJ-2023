6f27568177a798b0d5b39f51a0aa4b6affbeb752
Matthijs Nonkesgit config --global user.email mnonkes2@st.noorderpoort.nl <113519242+Lucifixr@users.noreply.github.com>
2023-02-04 09:38:10 +0100

    Slider en GUI
    ben nu bezig met mijn slider, nog niet gefinished

---
+++ TreeDefense-GGJ/Assets/Main/Scripts/MainMenuGUI/Scenes.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    public Canvas OptionsMenu;
+    public Canvas MainMenu;
+
+    public void Start()
+    {
+        OptionsMenu.enabled = false;
+        MainMenu.enabled = true;
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Main Level", LoadSceneMode.Single);
+    }
+    public void Options()
+    {
+        OptionsMenu.enabled = true;
+        MainMenu.enabled = false;
+    }
+
+    public void ReturnMM()
+    {
+        MainMenu.enabled = true;
+        OptionsMenu.enabled = false;
+    }    
+
+}
+
+
+
